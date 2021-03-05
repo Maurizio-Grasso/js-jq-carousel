9afbc7125219be7a6edca754d850708cc8721ded
maurizio <maurizio.grasso@gmail.com>
2021-03-05 17:08:43 +0100

    InputDaTastiera

--- main.js
+++ main.js
@@ -5,28 +5,53 @@ mainFunction();
 function mainFunction() {
     $('.prev').click( function() { SliderPrevImage(this); });
     $('.next').click( function() { SliderNextImage(this); });
+
+    $(document).keydown(function(keyPressed) {
+        if (keyPressed.keyCode == 37)
+            SliderPrevImage("byKeyboard");
+        else if (keyPressed.keyCode == 39)
+            SliderNextImage("byKeyboard");
+      }
+      );
 }
 
 function SliderPrevImage(object) {
-    var currentSliderImages = $(object).parent().find('.images'); 
-    var activeImage = $(currentSliderImages).find('.active');
 
-    activeImage.removeClass('active');
+    // Nella pagina potrebbero essere presenti diversi slider.
+    // Passo quindi come parametro alla funzione lo specifico elemendo del DOM
+    // Sul quale l'utente ha fatto click
 
-    if (activeImage.hasClass('first'))
-        $(currentSliderImages).find('img.last').addClass('active');
+    // Se la funzione è stata richiamata tramite tastiera
+    // L'immagine (pazienza...) cambierà su tutti gli slider presenti sulla pagina
+
+    if (object == "byKeyboard" )
+        var currentSlider = $('.slider-wrapper');   // Attiva tutti gli slider
     else
-        activeImage.prev('img').addClass('active');
+        var currentSlider = $(object).parent();   // Attiva Slider Specifico
+
+    var activeElement = $(currentSlider).find('.active');   //Immagine Attiva
+
+    activeElement.removeClass('active');
+
+    if (activeElement.hasClass('first'))
+        $(currentSlider).find('.last').addClass('active');
+    else
+        activeElement.prev().addClass('active');    
 }
 
 function SliderNextImage(object) {
-    var currentSliderImages = $(object).parent().find('.images'); 
-    var activeImage = $(currentSliderImages).find('.active');
 
-    activeImage.removeClass('active');
-
-    if (activeImage.hasClass('last'))
-        $(currentSliderImages).find('img.first').addClass('active');
+    if (object == "byKeyboard" )
+        var currentSlider = $('.slider-wrapper');   // Attiva tutti gli slider
     else
-        activeImage.next('img').addClass('active');
+        var currentSlider = $(object).parent();   // Attiva Slider Specifico
+
+    var activeElement = $(currentSlider).find('.active');       //Immagine Attiva
+
+    activeElement.removeClass('active');
+
+    if (activeElement.hasClass('last'))
+        $(currentSlider).find('.first').addClass('active');
+    else
+        activeElement.next().addClass('active');
 }
